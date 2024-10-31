2e249b0c0b9f8c2c29e70eae2f9199fdc80fcef9
Rocky☆Star <rocky-star22@outlook.com>
2024-10-31 23:08:45 +0800

    Add Tree Sitter recipes and hide pyim-isearch modeline

--- init.el
+++ init.el
@@ -64,6 +64,23 @@
 	(t
 	 (setopt completions-detailed t)
 	 (setopt fido-vertical-mode t)))
+
+  (setq treesit-language-source-alist
+	'((bash "https://github.com/tree-sitter/tree-sitter-bash")
+	  (cmake "https://github.com/uyha/tree-sitter-cmake")
+	  (css "https://github.com/tree-sitter/tree-sitter-css")
+	  (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+	  (go "https://github.com/tree-sitter/tree-sitter-go")
+	  (html "https://github.com/tree-sitter/tree-sitter-html")
+	  (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+	  (json "https://github.com/tree-sitter/tree-sitter-json")
+	  (make "https://github.com/alemuller/tree-sitter-make")
+	  (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+	  (python "https://github.com/tree-sitter/tree-sitter-python")
+	  (toml "https://github.com/tree-sitter/tree-sitter-toml")
+	  (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+	  (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+	  (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
   :config
   (if (>= (display-color-cells) 256)
       (load-theme 'leuven t))
@@ -192,6 +209,7 @@
 
 (use-package pyim-cregexp-utils
   :after pyim
+  :diminish pyim-isearch-mode
   :config
   (pyim-isearch-mode 1))
 
