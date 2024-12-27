ca51dbf3803aa35da7e961b20f9021e2fe984be1
Rocky☆Star <rocky-star22@outlook.com>
2024-12-27 08:57:55 +0800

    Add dminish to selected packages

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(auctex avy corfu diff-hl eglot-booster fringe-scale guru-mode pyim pyim-basedict pyim-tsinghua-dict which-key))
+ '(package-selected-packages '(auctex avy corfu diff-hl diminish eglot-booster fringe-scale guru-mode pyim pyim-basedict pyim-tsinghua-dict which-key))
  '(package-vc-selected-packages
    '((eglot-booster :vc-backend Git :url
 		    "https://github.com/jdtsmith/eglot-booster.git")
